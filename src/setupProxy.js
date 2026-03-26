acf81a84e88bb7d218e2886d16659ae6c55e83aa
Sovannrathh <sovannrath092@gmail.com>
2026-03-26 10:22:28 +0700

    remove all the unessesary

--- src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-const proxy = require("http-proxy-middleware")
-
-module.exports = function(app) {
-    app.use(proxy("/_matrix/maubot/v1", { target: process.env.PROXY || "http://localhost:29316" }))
-    app.use(proxy("/_matrix/maubot/v1/logs", { target: process.env.PROXY || "http://localhost:29316", ws: true }))
-}
